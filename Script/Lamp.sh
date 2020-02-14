829b3f37f1a83aa25915ac84cc833fc5c0b95e03
asift91 <60592188+asift91@users.noreply.github.com>
2020-02-14 17:30:55 +0530

    edited script

---
+++ Script/Lamp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get -y update
+dbpass=Q!w2e3r4
+export DEBIAN_FRONTEND=noninteractive
+echo "mysql-server-5.7 mysql-server/root_password password" $dbpass
+echo "mysql-server-5.7 mysql-server/root_password_again password" $dbpass
+sudo apt-get -y install mysql-server-5.7
+sudo apt-get -qq -y install apache2 libapache2-mod-php7.2 php7.2
+sudo service apache2 restart
