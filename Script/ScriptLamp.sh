e35e9f76697e0da785e6950eb92b2b01bce9174b
asift91 <60592188+asift91@users.noreply.github.com>
2020-02-14 16:49:46 +0530

    adding script

--- Script/ScriptLamp.sh
+++ Script/ScriptLamp.sh
@@ -4,5 +4,6 @@ dbpass=Q!w2e3r4
 export DEBIAN_FRONTEND=noninteractive
 echo "mysql-server-5.7 mysql-server/root_password password" $dbpass 
 echo "mysql-server-5.7 mysql-server/root_password_again password" $dbpass
+sudo apt-get -y install mysql-server-5.7
 sudo apt-get -y install apache2 php7
 sudo service apache2 restart
