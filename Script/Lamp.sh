7c81e1d0d420d8a3027a7116ca417d112a72459d
unknown <asift@italentdigital.com>
2020-02-17 17:39:41 +0530

    added html content in the script for testing purpose

--- Script/Lamp.sh
+++ Script/Lamp.sh
@@ -6,4 +6,6 @@ echo "mysql-server-5.7 mysql-server/root_password password" $dbpass
 echo "mysql-server-5.7 mysql-server/root_password_again password" $dbpass
 sudo apt-get -y install mysql-server-5.7
 sudo apt-get -qq -y install apache2 libapache2-mod-php7.2 php7.2
+# write some HTML
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 sudo service apache2 restart
